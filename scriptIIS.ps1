e3ab6736c653eb1dd130e8ea337437a765c6a543
pramod1990 <pramodkambhampati1990@gmail.com>
2018-02-20 16:06:36 -0600

    Update scriptIIS.ps1

--- scriptIIS.ps1
+++ scriptIIS.ps1
@@ -3,3 +3,5 @@ $setpolicy = "Read, Script,Execute"
 Set-WebConfiguration //System.webServer/handlers -metadata overrideMode -value Allow -PSPath IIS:/ -verbose
 Set-WebConfiguration -filter "/system.webServer/handlers/@AccessPolicy" -PSPath $site -value $setpolicy -verbose
 Set-WebConfigurationProperty -filter /system.webServer/directoryBrowse -name enabled -PSPath 'IIS:\Sites\Default Web Site' -value true
+
+Set-WebConfigurationProperty -filter system.webServer/security/authentication/anonymousAuthentication -name enabled -PSPath 'IIS:\Sites\' -value true
